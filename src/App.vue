600fff4e0b256b5736bf87533acd22204c72c00b
Michele Arcopinto <150271708+michelearcopinto@users.noreply.github.com>
2024-02-06 17:57:28 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+    import ExampleName from './components/ExampleName.vue'
+
+    export default {
+        components: {
+            ExampleName
+        }
+    }
+</script>
+
+<template>
+    <h1>Hello World</h1>
+
+    <ExampleName />
+</template>
+
+<style>
+    h1 {
+        color: blue;
+    }
+</style>
---
+++ src/components/ExampleName.vue
@@ -0,0 +1,17 @@
+<script>
+    // import
+
+    export default{
+        name: "ExampleName"
+    }
+</script>
+
+<template>
+    <h1>Hi I'm component</h1>
+</template>
+
+<style scoped>
+    h1 {
+        color: red;
+    }
+</style>
